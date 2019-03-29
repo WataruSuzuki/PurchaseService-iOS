e52747a3c4df47948d4e354389f5f8d48197014d
WataruSuzuki <wataru0406@gmail.com>
2019-03-29 22:20:00 +0900

    Created xcode project

---
+++ PurchaseService/PurchaseService.h
@@ -0,0 +1,19 @@
+//
+//  PurchaseService.h
+//  PurchaseService
+//
+//  Created by 鈴木航 on 2019/06/25.
+//  Copyright © 2019 WataruSuzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PurchaseService.
+FOUNDATION_EXPORT double PurchaseServiceVersionNumber;
+
+//! Project version string for PurchaseService.
+FOUNDATION_EXPORT const unsigned char PurchaseServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PurchaseService/PublicHeader.h>
+
+
